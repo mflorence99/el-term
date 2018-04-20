c42a6e2d3196441834b335c3d712438863a4e1f7
Mark Florence <mflo999@gmail.com>
2018-04-20 16:11:26 -0400

    Powerline-ready

--- main/main.ts
+++ main/main.ts
@@ -6,36 +6,37 @@ import { BrowserWindow, app, ipcMain } from 'electron';
 /**
  * Electron event dispatcher
  */
+
 const isDev = process.env['DEV_MODE'] === '1';
-
 const pids = [];
-
 let theWindow: BrowserWindow;
 
-function sendBounds() {
-  theWindow.webContents.send('bounds', theWindow.getBounds());
-}
-
 app.on('ready', () => {
-  if (isDev)
-    require('devtron').install();
   theWindow = new BrowserWindow({
     width: 800,
     height: 600,
     resizable: true
   });
-  // theWindow.loadURL('http://localhost:4200');
-  theWindow.loadURL(url.format({
-    // TODO: temporary -- not deploying from dist to get hot reload
-    hostname: 'localhost',
-    pathname: path.join(),
-    port: 4200,
-    protocol: 'http:',
-    // pathname: path.join(__dirname, 'index.html'),
-    // protocol: 'file:',
-    slashes: true
-  }));
+  if (isDev) {
+    require('devtron').install();
+    theWindow.loadURL(url.format({
+      hostname: 'localhost',
+      pathname: path.join(),
+      port: 4200,
+      protocol: 'http:',
+      slashes: true
+    }));
+  }
+  else {
+    theWindow.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+  }
   theWindow.setMenu(null);
+  const sendBounds = () =>
+    theWindow.webContents.send('bounds', theWindow.getBounds());
   theWindow.on('move', sendBounds);
   theWindow.on('resize', sendBounds);
 });
